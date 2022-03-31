b29bbee594c612087989ff6276ae906a9cce9ecf
Phong Nguyen Hong <intern.nhphong1@tma.com.vn>
2022-03-31 17:11:20 +0700

    Cua Phogn nha

---
+++ src/Icon.js
@@ -0,0 +1,13 @@
+import {
+    PlusCircleOutlined
+  } from "@ant-design/icons";
+import React from "react";
+class Icon extends React.Component{
+    render(){
+        return(<>
+               <PlusCircleOutlined style={{ fontSize: 15, color: "#00f500" }}/>
+        </>);
+    }
+}
+
+export default Icon;
