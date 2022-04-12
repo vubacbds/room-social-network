bb7038ee4053f1a79c9dab7b433fe930d697d8ba
Vu Bac <vubacbds@gmail.com>
2022-04-12 13:20:31 +0700

    Code trang Admin

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Suspense } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate, Navigate, Link , Redirect} from "react-router-dom";
 import { Spin } from "antd";
 import "antd/dist/antd.css";
 import "./App.scss";
 import NotFound from "./components/not-found";
+import {Home, UserManagement, PostManagement, Test} from './features/dashboard'
 
 //Lazy loading
 const HomePage = React.lazy(() => import("./features/main"));
+const AdminPage = React.lazy(() => import("./features/dashboard"));
 
 function App() {
+  
   return (
     <div className="app">
       <Suspense
@@ -23,11 +26,22 @@ function App() {
             <Route path="/" element={<Navigate to="/room-social-network" replace />} />
             <Route path="/room-social-network/*" element={<HomePage/>} />
             <Route path="*" element={<NotFound/>} />
+
+            <Route path="/room-social-network/admin" element={<AdminPage />}>           
+              <Route index element={<Navigate to="home" replace/>}/>
+              <Route path="home" element={<Home />} />
+              <Route path="quan-ly-nguoi-dung" element={<UserManagement />} />
+              <Route path="quan-ly-tin-dang" element={<PostManagement />} />
+              <Route path="giao-dien" element={<span>Tính năng đang được phát triển...</span>} />
+              <Route path="tu-khoa-hot" element={<span>Tính năng đang được phát triển...</span>} />
+           </Route>
+
+            <Route path="/room-social-network/admin/*" element={<NotFound />} />  
           </Routes>
         </BrowserRouter>
       </Suspense>
     </div>
   );
-}
+} 
 
 export default App;
---
+++ src/features/dashboard/index.jsx
@@ -0,0 +1,6 @@
+export {default} from './pages/dashboard'
+export {default as Home} from './pages/Home'
+export {default as UserManagement} from './pages/UserManagement'
+export {default as PostManagement} from './pages/PostManagement/index'
+
+
